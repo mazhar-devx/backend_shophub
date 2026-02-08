969310966b076fbebd13b1cd7d7983743f0026b3
Mazhar.devx <mazhar.devx@gmail.com>
2026-02-09 02:37:10 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+router.post('/google', authController.googleLogin);
 router.get('/logout', authController.logout);
 
 router.post('/forgotPassword', authController.forgotPassword);
