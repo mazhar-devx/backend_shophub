06ad61ea4836d756ac782eb421b17693c7b2e222
Mazhar.devx <mazhar.devx@gmail.com>
2026-02-11 02:30:07 +0500

    Update aiRoutes.js

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -3,5 +3,6 @@ const aiController = require('../controllers/aiController');
 const router = express.Router();
 
 router.post('/chat', aiController.getChatResponse);
+router.post('/product-guide', aiController.getProductGuideResponse);
 
 module.exports = router;
