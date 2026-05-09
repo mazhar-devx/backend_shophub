75824092d2dcd10de0af822510284394db5f0ad6
mazhar <mazhar.devx@gmail.com>
2026-05-09 21:53:32 +0500

    Fix Mongoose deprecation warnings

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,7 +33,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-    new: true,
+    returnDocument: 'after',
     runValidators: true
   });
 
@@ -95,7 +95,7 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
 
 exports.updateUser = catchAsync(async (req, res, next) => {
   const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
+    returnDocument: 'after',
     runValidators: true
   });
 
